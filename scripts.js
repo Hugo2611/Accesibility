ca5bd35031b3397ecbeab746d50b0431b20c79e0
matthieuDesbenoit <matt.desbe@orange.fr>
2024-05-22 09:59:13 +0200

    modif formulaire

--- scripts.js
+++ scripts.js
@@ -4,18 +4,18 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     const messageElement = document.getElementById('message');
     const nom = document.getElementById('nom').value.trim();
     const prenom = document.getElementById('prenom').value.trim();
+    const numeroRue = document.getElementById('numero_rue').value.trim();
+    const codePostal = document.getElementById('code_postal').value.trim();
+    const ville = document.getElementById('ville').value.trim();
+    const telephone = document.getElementById('téléphone').value.trim();
 
-    if (nom !== '' && prenom !== '') {
+    if (nom !== '' && prenom !== '' && numeroRue !== '' && codePostal !== '' && ville !== '') {
         messageElement.textContent = 'Votre message a été envoyé avec succès !';
         messageElement.classList.remove('hidden');
         messageElement.classList.add('success');
 
         setTimeout(function() {
             window.location.href = 'index.html';
-        }, 3000);
-    } else {
-        messageElement.textContent = 'Veuillez remplir les champs obligatoires.';
-        messageElement.classList.remove('hidden');
-        messageElement.classList.add('error');
-    }
+        }, 1000);
+    } 
 });
